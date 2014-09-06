3592e71a07727a1db0bca7ee7e481b9f6ea5faf0
root <eljhkrr@gmail.com>
2014-09-06 18:12:31 +0300

    Switched to new layout

--- js/app.js
+++ js/app.js
@@ -1,64 +1,13 @@
-var app = angular.module('myModule', ['ui.bootstrap']);
+var app = angular.module("myApp", ['']);
 
-app.controller("TabMenuController", ["$scope", function($scope){
-	
+/*
+app.controller("HolidayItemsController", ["$scope", function($scope){
+
+
 }]);
+*/
 
-app.controller("CardsController", ["$scope", "$modal", function($scope, $modal){
+app.controller("HolidayItemsController", function($scope){
 
-	$scope.cardModal = function(size){
-		var modalInstance = $modal.open({
-			templateUrl: 'cardModals.html'
-		});
-	};
 	
-}]);
-
-app.controller("NavController", ["$scope", "$modal", function($scope, $modal){
-
-	$scope.isCollapsed = true;
-
-	$scope.signinModal = function(size){
-
-		var modalInstance = $modal.open({
-			templateUrl: 'myModalContent.html',
-			size: size
-		});
-		console.log("called");
-		console.log($scope.showNavbar);
-		$scope.showNavbar = false;
-	};
-
-	$scope.removeModal = function(){
-		modalInstance.close();
-		alert('gone');
-	};
-	
-
-	//$scope.showNavbar = true;
-}]);
-
-app.directive("scroll", function($window){
-	return function(scope, element, attrs){
-		scope.visible = true;
-		angular.element($window).bind("scroll", function(){
-			console.log(this.pageYOffset);
-			if (this.pageYOffset > 200) {
-				element.removeClass('ng-hide');
-				element.addClass('ng-show');
-				//element.removeClass('navbar-invisible');
-				//element.set("ng-show", "false");
-				//console.log('gr8r!');
-				//scope.showNavbar = false;
-			} else if (this.pageYOffset < 200) {
-				//element.addClass('navbar-hide');
-				element.removeClass('ng-show');
-				element.addClass('ng-hide');
-
-			}
-			//if (this.pageYOffset >= 100) alert('nice!');
-		});
-		
-		//$scope.apply();
-	};
 });
